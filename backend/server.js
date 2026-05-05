a2d33a35e1fc43ab9ab0c45523466930b470523d
priyankahotkar <160253899+priyankahotkar@users.noreply.github.com>
2026-05-05 11:13:54 +0530

    Redploying backend

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "https://major-project-peach.vercel.app",
+  "https://major-project-peach.vercel.app/notes",
   process.env.FRONTEND_URL
 ];
 
